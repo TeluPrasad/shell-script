73fdb8b4f975b6e5fe7e6f28100ed2f6e1ee7a7a
prasad <prasad.telu2016@gmail.com>
2023-08-20 17:10:06 +0530

    Added comment in the section

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1=$1
-PERSON2=$2
+PERSON1=$1 #This is the first value from command line
+PERSON2=$2 #This is the second value from command line 
 echo "Hello am learning Devops with AWS"
 echo "$PERSON1: Hi $PERSON2, Good Morning"
 echo "$PERSON2: Good Mornining $PERSON1"

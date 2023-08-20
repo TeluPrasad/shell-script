6a56f8eab91e26dbbca1433bb9e7d080ea6a8fd5
prasad <prasad.telu2016@gmail.com>
2023-08-20 14:16:26 +0530

    Intoduced variables

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
 echo "Hello am learning Devops with AWS"
-echo "Rmaesh: Hi Suresh, Good Morning"
-echo "Suresh: Good Mornining Ramesh"
-echo "Ramesh: i am good, how are you" 
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Mornining $PERSON1"
+echo "$PERSON1: i am good, how are you" 

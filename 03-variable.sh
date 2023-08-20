10f5f51d0fb8d38ab3f7dd1fae9bdafe2bedaf3a
prasad <prasad.telu2016@gmail.com>
2023-08-20 14:43:12 +0530

    This is my variales without hard coded

---
+++ 03-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "Hello am learning Devops with AWS"
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Mornining $PERSON1"
+echo "$PERSON1: i am good, how are you" 

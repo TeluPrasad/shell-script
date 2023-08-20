a5ab1211ec64de7ba3ed8559d1b749ea8798b20d
prasad <prasad.telu2016@gmail.com>
2023-08-20 17:45:32 +0530

    added missed parameters

--- 04-variables-prompt.sh
+++ 04-variables-prompt.sh
@@ -2,8 +2,8 @@
 #This is the script to enter user name and password using prompt
 
 echo "Please enter the username"
-read USERNAME
+read -s USERNAME
 echo "user name is $USERNAME"
 echo "Please enter your password"
-read PASSWORD
+read -s PASSWORD
 echo "entered password is $PASSWORD"

9f9ad32a235ee69b411ca00c25b1c0b2bd32191c
prasad <prasad.telu2016@gmail.com>
2023-08-22 10:22:35 +0530

    Added to display second person and all person names

--- 05-array.sh
+++ 05-array.sh
@@ -2,4 +2,6 @@
 #this is the script to check  the array concept
 echo " This is the script to dispaly the array concept"
 PERSONS=("ramu" " raju" "raghu")
-echo " the first person is : ${PERSONS[0]}"
+echo " the first person name is : ${PERSONS[0]}"
+echo " The second person name is : ${PERSONS[1]}" # To display the second person name
+echo " All person names : ${PERSONS[@]}"

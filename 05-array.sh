054742159f9394e03b20589f6ff367151f9aee45
prasad <prasad.telu2016@gmail.com>
2023-08-22 10:16:08 +0530

    this is array script

--- 05-array.sh
+++ 05-array.sh
@@ -2,4 +2,4 @@
 #this is the script to check  the array concept
 echo " This is the script to dispaly the array concept"
 PERSONS=("ramu" " raju" "raghu")
-echo " the first person is : ${PERSONS[0]}
+echo " the first person is : ${PERSONS[0]}"
